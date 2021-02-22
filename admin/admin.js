1f443ea123647a0b80d6a75b3744100ad33b01eb
ChiaweiPeng <chiawei429789881@163.com>
2021-02-22 17:54:44 +0800

    完成编辑按钮，完善后台页面

--- admin/admin.js
+++ admin/admin.js
@@ -10,13 +10,15 @@ $(function () {
 
     // 刷新跳转到源页面
     window.onload = function () {
-        location.hash = ''
+        location.hash = '#list'
     }
+
+
 
     // 根据hash值改变内容区域
     $(window).bind('hashchange', function () {
+        
         let params = location.hash
-        // console.log(params)
 
         if (params.indexOf('list') > 0) {
             $('.content').removeClass('active')
@@ -41,6 +43,76 @@ $(function () {
                 emoji:true,
                 path: "../node_modules/editor.md/lib/"  // Autoload modules mode, codemirror, marked... dependents libs path
             });
+
+           
+        }
+        if( params.indexOf('edit') > 0) {
+            $('.small-nav').html('新建博客 > 更新博客')
+            // 当前在编辑页，先获取当前id博客内容
+            const id = location.hash.split('?')[1].slice(3)
+            const $title = $('.new-blog>>.blog-title')
+            const $content = $('.editormd-markdown-textarea')
+            const $subtitle = $('.new-blog>>.blog-subtitle')
+            const $type = $('.new-blog>>.blog-type')
+            const $select = $('.new-blog>>#type-sel')
+
+            let getUrl = '/api/blog/detail?id=' + id
+            get(getUrl).then( res => {
+                if(res.errno != 0) {
+                    alert('数据错误')
+                    return
+                }
+
+                const data = res.data || []
+                $title.val(data.title)
+                $content.val(data.content)
+                $subtitle.val(data.sub_title)
+                $type.val(data.type.slice(5))
+                // 注意目前仅支持 选择框为 Tech或Life
+                $select.val(data.type.substring(0,4))
+            })
+
+            // 添加取消编辑按钮
+            $('.new-blog').append($(`
+                <div id="close-update" title="点击取消编辑"><span class="glyphicon glyphicon-remove"></span></div> 
+            `))
+
+            $('.new-blog').delegate('#close-update', 'click' ,function(e) {
+                window.location.reload()
+            })
+
+            // 提交编辑按钮
+            $btnUpdate = $('.new-blog>>#update')
+            $('.new-blog>>.btn').css('display','none')
+            $btnUpdate.css('display','block')
+
+            // 提交编辑
+            $btnUpdate.click( () => {
+                const postUrl = '/api/blog/update?id=' + id
+                const title = $title.val().trim()
+                const content = $content.val().trim()
+                const sub_title = $subtitle.val().trim()
+                const type = $select.val() + ',' + $type.val().trim()
+
+                const postData = {
+                    title,
+                    content,
+                    sub_title,
+                    type
+                }
+
+                post(postUrl,postData).then(res => {
+                    if(res.errno != 0){
+                        alert('操作失败')
+                        return
+                    }
+
+                    alert('博客更新成功！')
+                    // 打开该更新的博客，并刷新后台页面
+                    window.open('/detail?id=' + id)
+                    location.href = '/admin/admin.html'
+                })
+            })
         }
         if (params.indexOf('other') > 0) {
             $('.content').removeClass('active')
@@ -88,8 +160,9 @@ $(function () {
                 <td class="blog-Id">${item.id}</td>
                 <td class="blog-Title">${item.title}</td>
                 <td class="blog-subtitle">${item.sub_title}</td>
+                <td class="blog-type">${item.type}</td>
                 <td class="blog-createtime">${getFormatDate(item.createtime)}</td>
-                <td><a href="javascript:;" id="open" class="${item.id}">打开</a> | <a href="javascript:;" id="edit">编辑</a> | <a href="javascript:;"
+                <td><a href="javascript:;" id="open" class="${item.id}">打开</a> | <a href="javascript:;" id="edit" class="${item.id}">编辑</a> | <a href="javascript:;"
                     id="delete" class="${item.id}">删除</a></td>
             </tr>
             `))
@@ -120,6 +193,12 @@ $(function () {
                 location.href = '/admin/admin.html'
             })
         }
+    })
+
+    // 更新博客
+    $('table').delegate('#edit', 'click', function(e) {
+        const id = $(this).attr('class')
+        location.hash = '#newBlog#edit?id=' + id
     })
 
     // 新建博客
